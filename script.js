1ec7393bc13c3350c1df74759c065cf0b6ad1b5e
João Carvalho <cjoao7610@gmail.com>
2024-07-30 14:35:24 -0300

    feat: styling error message and welcome screen to not appears in main content

--- script.js
+++ script.js
@@ -10,3 +10,7 @@ const errorMessage = document.getElementById('error-message');
 // define email and password values
 const myEmail = "email@email.com";
 const myPassword = "12345";
+
+// styling error message and welcome screen to not appears in main content
+errorMessage.style.display = "none";
+welcomeScreen.style.display = "none";
