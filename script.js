d5321e03da4a6d9d21201d4174438b302f01d509
João Carvalho <cjoao7610@gmail.com>
2024-07-30 14:33:35 -0300

    feat: get elements

---
+++ script.js
@@ -0,0 +1,8 @@
+// get elements
+const loginButton = document.getElementById('login-btn');
+const backButton = document.getElementById('back-btn');
+const usernameInput = document.getElementById('your-email');
+const passwordInput = document.getElementById('your-password');
+const loginContainer = document.querySelector('.container');
+const welcomeScreen = document.querySelector('.welcome-screen');
+const errorMessage = document.getElementById('error-message');
